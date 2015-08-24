75e1fb63c593034dd3d9357c59dec7178352ad60
Falkirks <me@falkirks.com>
2015-08-24 15:08:45 -0700

    Updates to PurePerms

--- src/rankup/permission/PurePerms.php
+++ src/rankup/permission/PurePerms.php
@@ -7,15 +7,27 @@ class PurePerms extends BasePermissionManager{
     public function addToGroup(Player $player, $group){
         if(!$this->checkReady()) return false;
         $ppGroup = $this->getAPI()->getGroup($group);
-		$this->getAPI()->setGroup($player, $ppGroup);
+		$this->getAPI()->getUser($player)->setGroup($ppGroup, null);
+        return true;
     }
     public function getGroup(Player $player){
         if(!$this->checkReady()) return false;		
 		$user = $this->getAPI()->getUser($player);
 		return $user->getGroup()->getName();
     }
+
+    /**
+     * getUsers() is not implemented in PurePerms. This method will NOT work.
+     * @param $name
+     * @return bool|void
+     */
     public function getPlayersInGroup($name){
-        // TODO: Implement getPlayersInGroup() method.
+        if(!$this->checkReady()) return false;
+        $group = $this->getAPI()->getGroup($name);
+        if($group !== null) {
+            return $group->getUsers();
+        }
+        return null;
     }
 
     /**
