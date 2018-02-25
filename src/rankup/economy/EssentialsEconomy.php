d1d0ff50c4f9f0905319de73ddb74319dc6f77e8
Jack M. Taylor <32965703+JackMD@users.noreply.github.com>
2018-02-25 23:16:15 +0500

    PHPDocs update

--- src/rankup/economy/EssentialsEconomy.php
+++ src/rankup/economy/EssentialsEconomy.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace rankup\economy;
 
 use EssentialsPEconomy\EssentialsPEconomy;
@@ -6,33 +7,72 @@ use EssentialsPEconomy\Providers\BaseEconomyProvider;
 use pocketmine\Player;
 use pocketmine\plugin\PluginBase;
 
-class EssentialsEconomy extends BaseEconomy{
-    public function give($amt, Player $player){
-        if(!$this->checkReady()) return false;
+class EssentialsEconomy extends BaseEconomy
+{
+    /**
+     * @param $amt
+     * @param Player $player
+     * @return bool
+     */
+    public function give($amt, Player $player)
+    {
+        if (!$this->checkReady()) return false;
         return $this->getAPI()->addToBalance($player, $amt);
     }
-    public function take($amt, Player $player){
-        if(!$this->checkReady()) return false;
 
-        return $this->getAPI()->subtractFromBalance($player, $amt);
-    }
-    public function setBal($amt, Player $player){
-        if(!$this->checkReady()) return false;
-        return $this->getAPI()->setBalance($player, $amt);
-    }
-    public function getBal(Player $player){
-        return $this->getAPI()->getBalance($player);
-    }
     /**
      * @return BaseEconomyProvider
      */
-    public function getAPI(){
+    public function getAPI()
+    {
         return $this->getPlugin()->getServer()->getPluginManager()->getPlugin("EssentialsPEconomy")->getProvider();
     }
-    public function isReady(){
+
+    /**
+     * @param $amt
+     * @param Player $player
+     * @return bool
+     */
+    public function take($amt, Player $player)
+    {
+        if (!$this->checkReady()) return false;
+
+        return $this->getAPI()->subtractFromBalance($player, $amt);
+    }
+
+    /**
+     * @param $amt
+     * @param Player $player
+     * @return bool
+     */
+    public function setBal($amt, Player $player)
+    {
+        if (!$this->checkReady()) return false;
+        return $this->getAPI()->setBalance($player, $amt);
+    }
+
+    /**
+     * @param Player $player
+     * @return mixed
+     */
+    public function getBal(Player $player)
+    {
+        return $this->getAPI()->getBalance($player);
+    }
+
+    /**
+     * @return bool
+     */
+    public function isReady()
+    {
         return $this->getPlugin()->getServer()->getPluginManager()->getPlugin("EssentialsPEconomy") instanceof PluginBase;
     }
-    public function getName(){
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
         return "EssentialsPE by LegendsOfMCPE";
     }
 }
