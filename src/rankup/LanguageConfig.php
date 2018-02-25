ef8b56c5de3038cd8e099db78cc9da2118e0019a
Jack M. Taylor <32965703+JackMD@users.noreply.github.com>
2018-02-25 23:24:19 +0500

    PHPDocs update

--- src/rankup/LanguageConfig.php
+++ src/rankup/LanguageConfig.php
@@ -8,11 +8,19 @@ class LanguageConfig
 {
     private $lang;
 
+    /**
+     * LanguageConfig constructor.
+     * @param Config $config
+     */
     public function __construct(Config $config)
     {
         $this->lang = $config->get('lang');
     }
 
+    /**
+     * @param $name
+     * @return mixed
+     */
     public function getLangSetting($name)
     {
         return $this->lang[$name];
