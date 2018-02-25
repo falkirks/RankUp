bc3fa0f23cc58f7adbde3541f979d4d29ba7c6d0
Jack M. Taylor <32965703+JackMD@users.noreply.github.com>
2018-02-25 23:19:33 +0500

    PHPDocs update

--- src/rankup/economy/Economy.php
+++ src/rankup/economy/Economy.php
@@ -1,45 +1,83 @@
 <?php
+
 namespace rankup\economy;
 
 use pocketmine\Player;
 use pocketmine\plugin\Plugin;
 use pocketmine\plugin\PluginBase;
 
-class Economy extends BaseEconomy{
-    public function give($amt, Player $player){
-        if(!$this->checkReady()) return false;
+class Economy extends BaseEconomy
+{
+    /**
+     * @param $amt
+     * @param Player $player
+     * @return bool
+     */
+    public function give($amt, Player $player)
+    {
+        if (!$this->checkReady()) return false;
         return ($this->getAPI()->addMoney($player, $amt) === 1);
-    }
-    public function take($amt, Player $player){
-        if(!$this->checkReady()) return false;
-        return ($this->getAPI()->reduceMoney($player, $amt) === 1);
-    }
-    public function setBal($amt, Player $player){
-        if(!$this->checkReady()) return false;
-        return ($this->getAPI()->setMoney($player, $amt) === 1);
-    }
-    public function getBal(Player $player){
-        if(!$this->checkReady()) return false;
-        //TODO there is probably a better way to do this
-        $money = $this->getAPI()->getAllMoney();
-        if(isset($money["money"][strtolower($player->getName())])){
-            return $money["money"][strtolower($player->getName())];
-        }
-        else{
-            return false;
-        }
     }
 
     /**
      * @return Plugin
      */
-    public function getAPI(){
+    public function getAPI()
+    {
         return $this->getPlugin()->getServer()->getPluginManager()->getPlugin("EconomyAPI");
     }
-    public function isReady(){
+
+    /**
+     * @param $amt
+     * @param Player $player
+     * @return bool
+     */
+    public function take($amt, Player $player)
+    {
+        if (!$this->checkReady()) return false;
+        return ($this->getAPI()->reduceMoney($player, $amt) === 1);
+    }
+
+    /**
+     * @param $amt
+     * @param Player $player
+     * @return bool
+     */
+    public function setBal($amt, Player $player)
+    {
+        if (!$this->checkReady()) return false;
+        return ($this->getAPI()->setMoney($player, $amt) === 1);
+    }
+
+    /**
+     * @param Player $player
+     * @return bool
+     */
+    public function getBal(Player $player)
+    {
+        if (!$this->checkReady()) return false;
+        //TODO there is probably a better way to do this
+        $money = $this->getAPI()->getAllMoney();
+        if (isset($money["money"][strtolower($player->getName())])) {
+            return $money["money"][strtolower($player->getName())];
+        } else {
+            return false;
+        }
+    }
+
+    /**
+     * @return bool
+     */
+    public function isReady()
+    {
         return ($this->getAPI() instanceof PluginBase);
     }
-    public function getName(){
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
         return "EconomyS by onebone";
     }
 }
