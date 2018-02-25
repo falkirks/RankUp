8a9ec3cc7eceecc4b3b5b8e7076b6b3548d6260d
Jack M. Taylor <32965703+JackMD@users.noreply.github.com>
2018-02-25 23:15:11 +0500

    PHPDocs update

--- src/rankup/economy/PocketMoney.php
+++ src/rankup/economy/PocketMoney.php
@@ -1,36 +1,75 @@
 <?php
+
 namespace rankup\economy;
 
 use pocketmine\Player;
 use pocketmine\plugin\PluginBase;
 
-class PocketMoney extends BaseEconomy{
-    public function give($amt, Player $player){
-        if(!$this->checkReady()) return false;
+class PocketMoney extends BaseEconomy
+{
+    /**
+     * @param $amt
+     * @param Player $player
+     * @return bool
+     */
+    public function give($amt, Player $player)
+    {
+        if (!$this->checkReady()) return false;
         return $this->getAPI()->grantMoney($player->getName(), $amt);
-    }
-    public function take($amt, Player $player){
-        if(!$this->checkReady()) return false;
-        return $this->getAPI()->grantMoney($player->getName(), -$amt);
-    }
-    public function setBal($amt, Player $player){
-        if(!$this->checkReady()) return false;
-        return $this->getAPI()->setMoney($player->getName(), $amt);
-    }
-    public function getBal(Player $player){
-        return $this->getAPI()->getMoney($player->getName());
     }
 
     /**
      * @return \PocketMoney\PocketMoney
      */
-    public function getAPI(){
+    public function getAPI()
+    {
         return $this->getPlugin()->getServer()->getPluginManager()->getPlugin("PocketMoney");
     }
-    public function isReady(){
+
+    /**
+     * @param $amt
+     * @param Player $player
+     * @return bool
+     */
+    public function take($amt, Player $player)
+    {
+        if (!$this->checkReady()) return false;
+        return $this->getAPI()->grantMoney($player->getName(), -$amt);
+    }
+
+    /**
+     * @param $amt
+     * @param Player $player
+     * @return bool
+     */
+    public function setBal($amt, Player $player)
+    {
+        if (!$this->checkReady()) return false;
+        return $this->getAPI()->setMoney($player->getName(), $amt);
+    }
+
+    /**
+     * @param Player $player
+     * @return mixed
+     */
+    public function getBal(Player $player)
+    {
+        return $this->getAPI()->getMoney($player->getName());
+    }
+
+    /**
+     * @return bool
+     */
+    public function isReady()
+    {
         return ($this->getAPI() instanceof PluginBase);
     }
-    public function getName(){
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
         return "PocketMoney by MinecrafterJPN";
     }
 }
