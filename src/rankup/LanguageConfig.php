9961d6fe0f92e814d7f8feabe796e0a988904ab5
Jack M. Taylor <32965703+JackMD@users.noreply.github.com>
2018-02-25 23:03:33 +0500

    Update LanguageConfig.php

--- src/rankup/LanguageConfig.php
+++ src/rankup/LanguageConfig.php
@@ -1,16 +1,20 @@
 <?php
+
 namespace rankup;
 
 use pocketmine\utils\Config;
 
-class LanguageConfig{
+class LanguageConfig
+{
     private $lang;
 
-    public function __construct(Config $config){
+    public function __construct(Config $config)
+    {
         $this->lang = $config->get('lang');
     }
 
-    public function getLangSetting($name){
+    public function getLangSetting($name)
+    {
         return $this->lang[$name];
     }
 }
