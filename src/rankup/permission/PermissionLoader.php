044a27d77a7072a79eab9089ccd92f5350383d67
Jack M. Taylor <32965703+JackMD@users.noreply.github.com>
2018-02-25 23:20:50 +0500

    PHPDocs update

--- src/rankup/permission/PermissionLoader.php
+++ src/rankup/permission/PermissionLoader.php
@@ -1,39 +1,54 @@
 <?php
+
 namespace rankup\permission;
 
 use rankup\RankUp;
 
-class PermissionLoader{
+class PermissionLoader
+{
     private $plugin;
     private $name;
-    public function __construct(RankUp $plugin){
+
+    /**
+     * PermissionLoader constructor.
+     * @param RankUp $plugin
+     */
+    public function __construct(RankUp $plugin)
+    {
         $this->plugin = $plugin;
     }
-    public function load(){
-        if($this->plugin->getConfig()->get('preferred-groupmanager') !== false){
+
+    /**
+     *
+     */
+    public function load()
+    {
+        if ($this->plugin->getConfig()->get('preferred-groupmanager') !== false) {
             $name = $this->plugin->getConfig()->get('preferred-groupmanager');
-            try{
+            try {
                 $permManager = new $name($this->plugin);
                 $this->name = $permManager->getName();
-                if($permManager instanceof BasePermissionManager){
-                    if($permManager->isReady()){
+                if ($permManager instanceof BasePermissionManager) {
+                    if ($permManager->isReady()) {
                         $this->plugin->setPermManager($permManager);
                         $this->plugin->getLogger()->info("Loaded " . $permManager->getName());
-                    }
-                    else{
+                    } else {
                         $this->plugin->getLogger()->critical("The preferred-groupmanager you specified is not loaded.");
                     }
                 }
-            }
-            catch(\ClassNotFoundException $e){
+            } catch (\ClassNotFoundException $e) {
                 $this->plugin->getLogger()->critical("The preferred-groupmanager you specified is not supported.");
             }
-        }
-        else{
+        } else {
             //TODO autoload PurePerms and RankUpDoesGroups
         }
     }
-    public function getName() : string{
-    	return $this->name;
-	}
+
+    /**
+     * @return string
+     */
+    public function getName(): string
+    {
+        return $this->name;
+    }
 }
